02f26a9a64c9e6f680c7d3efbb43cb7640fddf06
name <duwaynefrieslaar23@gmail.com>
2025-01-20 09:51:05 +0200

    added tailwindcss

---
+++ portfolio/src/components/intro.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Intro(){
+    return (
+        <>
+        <h1 className="text-4xl font-bold text-center text-blue-500">Duwayne Frieslaar</h1>
+        </>
+    )
+}
