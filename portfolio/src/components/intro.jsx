ce268a93e728e714db1cf1e8e05d755a42ed9fc7
name <duwaynefrieslaar23@gmail.com>
2025-01-20 10:59:35 +0200

    adjusted image path

--- portfolio/src/components/intro.jsx
+++ portfolio/src/components/intro.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Intro(){
     return (
         <>
+        <img src="/images/Me.png" className='' />
         <h1 className="text-4xl font-bold text-center text-blue-500">Duwayne Frieslaar</h1>
         </>
     )
